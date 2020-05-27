6a9cd9a6d1d562e32451c3ce917504879fb5ae93
Andy Cate <and164v@gmail.com>
2020-05-26 22:37:41 -0700

    Basic user api working

--- backend/database.js
+++ backend/database.js
@@ -2,17 +2,17 @@ const fs = require('fs');
 const { Pool } = require('pg');
 const pool = new Pool();
 
-const USER_TABLE_CREATE = `create table users(firstName text default '',
-                                           lastName text default '',
-                                           calnetID text primary key,
+const USER_TABLE_CREATE = `create table users(firstname text default '',
+                                           lastname text default '',
+                                           calnetid text primary key,
                                            email text unique not null,
                                            phone text unique null,
-                                           dateJoined timestamptz not null default now(),
-                                           queueNumber serial unique not null,
-                                           lastSignIn timestamptz not null default now(),
+                                           datejoined timestamptz not null default now(),
+                                           queuenumber serial unique not null,
+                                           lastsignin timestamptz not null default now(),
                                            admin bool not null default 'f',
-                                           alertEmail bool not null default 't',
-                                           alertPhone bool not null default 'f');`;
+                                           alertemail bool not null default 't',
+                                           alertphone bool not null default 'f');`;
 const USER_TABLE_EXISTS = `select exists (select from information_schema.tables where table_name='users');`;
 
 const getAbort = (client) => {
@@ -76,17 +76,17 @@ module.exports.verifyTables = () => {
     return Promise.all([userTablePromise, sessionTablePromise]);
 }
 
-const GET_USER_BY_ID_QUERY = 'select * from users where calnetID=$1';
+const GET_USER_BY_ID_QUERY = 'select * from users where calnetid=$1';
 module.exports.getUserByID = id => {
     return pool.query(GET_USER_BY_ID_QUERY, [id]).then(res => {
         return res.rows[0];
     }).catch(err => {
-        console.error('failed getting user from calnetID!', err.stack);
+        console.error('failed getting user from calnetid!', err.stack);
         return err;
     });
 }
 
-const INSERT_USER_QUERY = 'insert into users (calnetID, email) values ($1, $2)';
+const INSERT_USER_QUERY = 'insert into users (calnetid, email) values ($1, $2)';
 module.exports.insertUser = id => {
     return pool.query(INSERT_USER_QUERY, [id, `${id}@berkeley.edu`]).then(res => {
         return true;
@@ -95,7 +95,7 @@ module.exports.insertUser = id => {
     });
 }
 
-const CONTAINS_USER_QUERY = 'select count(distinct calnetID) from users where calnetID=$1';
+const CONTAINS_USER_QUERY = 'select count(distinct calnetid) from users where calnetid=$1';
 module.exports.containsUser = id => {
     return pool.query(CONTAINS_USER_QUERY, [id]).then(res => {
         return res.rows[0].count > 0;
@@ -104,7 +104,7 @@ module.exports.containsUser = id => {
     });
 }
 
-const UPDATE_LAST_USER_SIGNIN = 'update users set lastSignIn=localtimestamp where calnetID=$1';
+const UPDATE_LAST_USER_SIGNIN = 'update users set lastsignin=localtimestamp where calnetid=$1';
 module.exports.updateLastUserSignin = id => {
     return pool.query(UPDATE_LAST_USER_SIGNIN, [id]).then(res => {
         return true;
@@ -113,12 +113,50 @@ module.exports.updateLastUserSignin = id => {
     });
 }
 
-const CHECK_ALL_USER_INFO = 'select firstName, lastName from users where calnetID=$1';
+const CHECK_ALL_USER_INFO = 'select firstname, lastname from users where calnetid=$1';
 module.exports.checkAllUserInfoPresent = id => {
     return pool.query(CHECK_ALL_USER_INFO, [id]).then(res => {
         return res.rows[0].firstname !== '' &&
                res.rows[0].lastname !== '';
     }).catch(err => {
         return err;
+    });
+}
+
+// update users
+const UPDATE_USER_FIRSTNAME = 'update users set firstname=$1 where calnetid=$2';
+module.exports.updateFirstName = (id, firstname) => {
+    return pool.query(UPDATE_USER_FIRSTNAME, [firstname, id]).then(res => {
+        return res.rowCount > 0;
+    });
+}
+const UPDATE_USER_LASTNAME = 'update users set lastname=$1 where calnetid=$2';
+module.exports.updateLastName = (id, lastname) => {
+    return pool.query(UPDATE_USER_LASTNAME, [lastname, id]).then(res => {
+        return res.rowCount > 0;
+    });
+}
+const UPDATE_USER_EMAIL = 'update users set email=$1 where calnetid=$2';
+module.exports.updateEmail = (id, email) => {
+    return pool.query(UPDATE_USER_EMAIL, [email, id]).then(res => {
+        return res.rowCount > 0;
+    });
+}
+const UPDATE_USER_PHONE = 'update users set phone=$1 where calnetid=$2';
+module.exports.updatePhone = (id, phone) => {
+    return pool.query(UPDATE_USER_PHONE, [phone, id]).then(res => {
+        return res.rowCount > 0;
+    });
+}
+const UPDATE_USER_ALERTEMAIL = 'update users set alertemail=$1 where calnetid=$2';
+module.exports.updateAlertEmail = (id, alertemail) => {
+    return pool.query(UPDATE_USER_ALERTEMAIL, [alertemail, id]).then(res => {
+        return res.rowCount > 0;
+    });
+}
+const UPDATE_USER_ALERTPHONE = 'update users set alertphone=$1 where calnetid=$2';
+module.exports.updateAlertPhone = (id, alertphone) => {
+    return pool.query(UPDATE_USER_ALERTPHONE, [alertphone, id]).then(res => {
+        return res.rowCount > 0;
     });
 }
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,27 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const PNF = require('google-libphonenumber').PhoneNumberFormat;
+const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
 
 const Cas = require('../../cas');
-const { containsUser, insertUser, updateLastUserSignin, checkAllUserInfoPresent } = require('../../database');
+const { containsUser,
+        insertUser,
+        updateLastUserSignin,
+        checkAllUserInfoPresent,
+        getUserByID,
+        updateFirstName,
+        updateLastName,
+        updateEmail,
+        updatePhone,
+        updateAlertEmail,
+        updateAlertPhone } = require('../../database');
 
 router.get('/login', Cas.bounce, (request, response) => {
-    const calnetID = request.session.cas_user;
-    return containsUser(calnetID).then(res => {
+    const calnetid = request.session.cas_user;
+    return containsUser(calnetid).then(res => {
         if(!res) {
             // insert new user
-            return insertUser(calnetID).then(res => {
+            return insertUser(calnetid).then(res => {
                 response.redirect('/newuser');
             }).catch(err => {
-                console.error(`Error inserting new user ${calnetID}`);
+                console.error(`Error inserting new user ${calnetid}`);
                 console.error(err.stack);
                 response.status(500);
                 return err;
             });
         } else {
             // update last signin
-            return updateLastUserSignin(calnetID).then(res => {
+            return updateLastUserSignin(calnetid).then(res => {
                 // redirect to dashboard
-                return checkAllUserInfoPresent(calnetID);
+                return checkAllUserInfoPresent(calnetid);
             }).then(res => {
                 if(res) {
                     response.redirect('/dashboard');
@@ -29,14 +41,14 @@ router.get('/login', Cas.bounce, (request, response) => {
                     response.redirect('/newuser');
                 }
             }).catch(err => {
-                console.error(`Error updating last signin date for user ${calnetID}`);
+                console.error(`Error updating last signin date for user ${calnetid}`);
                 console.error(err.stack);
                 response.status(500);
                 return err;
             });
         }
     }).catch(err => {
-        console.error(`error checking if user ${calnetID} exists!`);
+        console.error(`error checking if user ${calnetid} exists!`);
         console.error(err.stack);
         response.status(500);
     });
@@ -44,10 +56,118 @@ router.get('/login', Cas.bounce, (request, response) => {
 
 router.get('/logout', Cas.logout);
 
-router.get('/get_user_profile', Cas.block, (request, response) => {
+router.get('/get/profile', Cas.block, (request, response) => {
+    // returns a single row from the database
+    const calnetid = request.session.cas_user;
+    getUserByID(calnetid).then(res => {
+        response.json(res);
+    }).catch(err => {
+        response.status(500);
+    });
 });
 
-router.post('/set_user_profile', Cas.block, (request, response) => {
+// sets any of the following parameters:
+// - first or last name
+// - email
+// - phone
+// - alertemail
+// - alertphone
+router.post('/set/firstname', Cas.block, (request, response) => {
+    const calnetid = request.session.cas_user;
+    if(!request.body.firstname) {
+        return response.status(400).json({error: 'No firstname parameter'});
+    }
+    updateFirstName(calnetid, request.body.firstname).then(res => {
+        if(res) {
+            return response.status(200).json({});
+        }
+        return response.status(500);
+    }).catch(err => {
+        console.error(`Error setting firstname for ${calnetid}`);
+        console.error(err.stack);
+    });
+});
+router.post('/set/lastname', Cas.block, (request, response) => {
+    const calnetid = request.session.cas_user;
+    if(!request.body.lastname) {
+        return response.status(400).json({error: 'No lastname parameter'});
+    }
+    updateLastName(calnetid, request.body.lastname).then(res => {
+        if(res) {
+            return response.status(200).json({});
+        }
+        return response.status(500);
+    }).catch(err => {
+        console.error(`Error setting lastname for ${calnetid}`);
+        console.error(err.stack);
+    });
+});
+router.post('/set/email', Cas.block, (request, response) => {
+    const calnetid = request.session.cas_user;
+    if(!request.body.email) {
+        return response.status(400).json({error: 'No email parameter'});
+    }
+    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(request.body.email)) {
+        return response.status(400).json({error: 'Email invalid'});
+    }
+    updateEmail(calnetid, request.body.email).then(res => {
+        if(res) {
+            return response.status(200).json({});
+        }
+        return response.status(500);
+    }).catch(err => {
+        console.error(`Error setting email for ${calnetid}`);
+        console.error(err.stack);
+    });
+});
+router.post('/set/phone', Cas.block, (request, response) => {
+    const calnetid = request.session.cas_user;
+    if(!request.body.phone) {
+        return response.status(400).json({error: 'No phone parameter'});
+    }
+    const phone = phoneUtil.parse(request.body.phone, 'US');
+    if(!phoneUtil.isValidNumber(phone)) {
+        return response.status(400).json({error: 'invalid phone number'});
+    }
+    updatePhone(calnetid, phoneUtil.format(phone, PNF.E164)).then(res => {
+        if(res) {
+            return response.status(200).json({});
+        }
+        return response.status(500);
+    }).catch(err => {
+        console.error(`Error setting phone for ${calnetid}`);
+        console.error(err.stack);
+    });
+});
+router.post('/set/alertemail', Cas.block, (request, response) => {
+    const calnetid = request.session.cas_user;
+    if(typeof request.body.alertemail !== 'boolean') {
+        return response.status(400).json({error: 'No alertemail parameter'});
+    }
+    updateAlertEmail(calnetid, request.body.alertemail).then(res => {
+        if(res) {
+            return response.status(200).json({});
+        }
+        return response.status(500);
+    }).catch(err => {
+        console.error(`Error setting alertemail for ${calnetid}`);
+        console.error(err.stack);
+    });
+});
+router.post('/set/alertphone', Cas.block, (request, response) => {
+    const calnetid = request.session.cas_user;
+    if(typeof request.body.alertphone !== 'boolean') {
+        return response.status(400).json({error: 'No alertphone parameter'});
+    }
+    updateAlertPhone(calnetid, request.body.alertphone).then(res => {
+        if(res) {
+            return response.status(200).json({});
+        }
+        return response.status(500);
+    }).catch(err => {
+        console.error(`Error setting alertphone for ${calnetid}`);
+        console.error(err.stack);
+    });
 });
 
 module.exports = router;
