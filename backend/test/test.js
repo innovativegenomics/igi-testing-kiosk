0d6db2f22cbaa18bc2c2f816983df4f0f60f8f75
Andy Cate <and164v@gmail.com>
2020-05-30 14:23:19 -0700

    Change postgres config to use keys.js

--- backend/test/test.js
+++ backend/test/test.js
@@ -2,7 +2,13 @@ const assert = require('assert');
 const path = require('path');
 require('dotenv').config({path: path.resolve(process.cwd(), '.env.dev')});
 const { Pool } = require('pg');
-const pool = new Pool();
+const pool = new Pool({
+    user: require('../config/keys').pg.pguser,
+    host: require('../config/keys').pg.pghost,
+    database: require('../config/keys').pg.pgdatabase,
+    password: require('../config/keys').pg.pgpassword,
+    port: require('../config/keys').pg.pgport,
+});
 const moment = require('moment');
 
 const { Settings } = require('../database/settingsActions');
