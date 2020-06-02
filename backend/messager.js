042d0f08492fbea25da1f39b5b9c62ddd6e5c978
Andy Cate <and164v@gmail.com>
2020-06-01 21:41:42 -0700

    Uncomment email code

--- backend/messager.js
+++ backend/messager.js
@@ -45,27 +45,27 @@ To change or cancel this appointment, log into your testing account.`,
 }
 
 module.exports.sendConfirmEmail = (email, uid, day, timeStart, timeEnd, location, locationLink) => {
-    // return qrcode.toDataURL(uid).then(res => {
-    //     return transport.sendMail({
-    //         from: config.email.user,
-    //         to: email,
-    //         subject: `Testing Appointment Confirmation for ${day}`,
-    //         // text: 'Please enable html messages to view this email',
-    //         html: `<h1>Appointment Confirmation for ${day}</h1>
-    //                <h2>Please arrive at <a href='${locationLink}'>${location}</a> between ${timeStart} and ${timeEnd}</h2>
-    //                <h2>Bring your phone or another device to display the QR Code below</h2>
-    //                <b><img src="cid:qrcode" width="300" height="300"/></b>
-    //                <br />
-    //                <b>If the above image doesn't show correctly, provide this reference number at the kiosk: ${uid}</b>`,
-    //         attachments: [
-    //             {
-    //                 filename: 'qrcode.png',
-    //                 content: res.split('base64,')[1],
-    //                 encoding: 'base64',
-    //                 cid: 'qrcode'
-    //             }
-    //         ]
-    //     });
-    // });
+    return qrcode.toDataURL(uid).then(res => {
+        return transport.sendMail({
+            from: config.email.user,
+            to: email,
+            subject: `Testing Appointment Confirmation for ${day}`,
+            // text: 'Please enable html messages to view this email',
+            html: `<h1>Appointment Confirmation for ${day}</h1>
+                   <h2>Please arrive at <a href='${locationLink}'>${location}</a> between ${timeStart} and ${timeEnd}</h2>
+                   <h2>Bring your phone or another device to display the QR Code below</h2>
+                   <b><img src="cid:qrcode" width="300" height="300"/></b>
+                   <br />
+                   <b>If the above image doesn't show correctly, provide this reference number at the kiosk: ${uid}</b>`,
+            attachments: [
+                {
+                    filename: 'qrcode.png',
+                    content: res.split('base64,')[1],
+                    encoding: 'base64',
+                    cid: 'qrcode'
+                }
+            ]
+        });
+    });
     return Promise.resolve();
 }
