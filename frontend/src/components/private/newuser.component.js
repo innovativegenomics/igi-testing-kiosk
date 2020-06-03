42f87695c64f6951c877a00e390c94234048e906
Andy Cate <and164v@gmail.com>
2020-06-02 19:39:34 -0700

    Fix phone issues

--- frontend/src/components/private/newuser.component.js
+++ frontend/src/components/private/newuser.component.js
@@ -13,43 +13,44 @@ class UserInfo extends Component {
             <div className='card'>
                 <div className='card-body'>
                     <h5 className='card-title text-center'>New User Info</h5>
+                    <div className='row'>
+                        <div className='col'>
+                            <p className='lead'>
+                                Please make sure your name matches the name on your Cal ID card.
+                            </p>
+                        </div>
+                    </div>
                     <form>
-                        <div className='form-group row'>
-                            <label htmlFor='firstname' className='col-3 col-form-label'>First Name</label>
-                            <div className='col-9'>
-                                <input type='text' className='form-control' id='firstname' placeholder='firstname' onChange={this.props.updateFirstName} value={this.props.firstname}/>
+                        <div className='form-group row required'>
+                            <label htmlFor='firstname' className='col-md-3 col-form-label'>First Name</label>
+                            <div className='col-md-9'>
+                                <input type='text' className='form-control' id='firstname' placeholder='firstname' onChange={this.props.updateFirstName} value={this.props.firstname} autoComplete={'off'} autoComplete='off' autoCorrect='off' autoCapitalize='none'/>
+                            </div>
+                        </div>
+                        <div className='form-group row required'>
+                            <label htmlFor='lastname' className='col-md-3 col-form-label'>Last Name</label>
+                            <div className='col-md-9'>
+                                <input type='text' className='form-control' id='lastname' placeholder='lastname' onChange={this.props.updateLastName} value={this.props.lastname} autoComplete={'off'} autoComplete='off' autoCorrect='off' autoCapitalize='none'/>
+                            </div>
+                        </div>
+                        <div className='form-group row required'>
+                            <label htmlFor='email' className='col-md-3 col-form-label'>Email</label>
+                            <div className='col-md-9'>
+                                <input type='email' className='form-control' id='email' placeholder='email' onChange={this.props.updateEmail} value={this.props.email} autoComplete={'off'} autoComplete='off' autoCorrect='off' autoCapitalize='none'/>
                             </div>
                         </div>
                         <div className='form-group row'>
-                            <label htmlFor='lastname' className='col-3 col-form-label'>Last Name</label>
-                            <div className='col-9'>
-                                <input type='text' className='form-control' id='lastname' placeholder='lastname' onChange={this.props.updateLastName} value={this.props.lastname}/>
-                            </div>
-                        </div>
-                        <div className='form-group row'>
-                            <label htmlFor='email' className='col-3 col-form-label'>Email</label>
-                            <div className='col-9'>
-                                <input type='email' className='form-control' id='email' placeholder='email' onChange={this.props.updateEmail} value={this.props.email}/>
-                            </div>
-                        </div>
-                        <div className='form-group row'>
-                            <label htmlFor='phone' className='col-3 col-form-label'>Phone(mobile)</label>
-                            <div className='col-9'>
-                                <input type='text' className='form-control' id='phone' placeholder='(123) 456 7891' onChange={this.props.updatePhone} value={this.props.phone}/>
-                            </div>
-                        </div>
-                        <div className='form-group row'>
-                            <label htmlFor='confirmCleared' className='col-8 col-form-label'>Have you been cleared to return to the campus?</label>
-                            <div className='col-1 p-2'>
-                                <input type='checkbox' className='form-check-input' id='confirmCleared'/>
+                            <label htmlFor='phone' className='col-md-3 col-form-label'>Phone(mobile)</label>
+                            <div className='col-md-9'>
+                                <input type='text' className='form-control' id='phone' placeholder='(123) 456 7891' onChange={this.props.updatePhone} value={this.props.phone} autoComplete='off' autoCorrect='off' autoCapitalize='none'/>
                             </div>
                         </div>
                     </form>
                     <div className='row'>
-                        <div className='col-2'>
+                        <div className='col-md-2'>
                             <button className='btn btn-primary' onClick={this.props.saveUserData} disabled={!this.props.isValid}>Save</button>
                         </div>
-                        <div className='col-9'>
+                        <div className='col-md-9'>
                             <p className='m-0'>By clicking 'Save' you agree to having your information stored in a secure server, and that it will be provided to UHS upon request.</p>
                         </div>
                     </div>
@@ -83,8 +84,10 @@ class NewUser extends Component {
         var isValidNumber = false;
         try {
             isValidNumber = PhoneNumberUtil.getInstance().isValidNumber(PhoneNumberUtil.getInstance().parse(stateCopy.phone, 'US'));
+            this.setState({alertphone: true});
         } catch(err) {
             isValidNumber = (stateCopy.phone === '');
+            this.setState({alertphone: false});
         }
         const valid = stateCopy.firstname !== '' &&
                 stateCopy.lastname !== '' &&
@@ -120,7 +123,7 @@ class NewUser extends Component {
                 <div style={{backgroundColor: '#eeeeee'}}>
                     <div className='container'>
                         <div className='row justify-content-center'>
-                            <div className='col-8 p-3'>
+                            <div className='col-md-8 p-3'>
                                 <UserInfo firstname={this.state.firstname}
                                             lastname={this.state.lastname}
                                             email={this.state.email}
