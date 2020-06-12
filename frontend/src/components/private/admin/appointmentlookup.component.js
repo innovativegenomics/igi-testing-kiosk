18d62a57df38490e190e360a24f4983b8a3b1236
Andy Cate <and164v@gmail.com>
2020-06-12 13:22:57 -0700

    frontend start

--- frontend/src/components/private/admin/appointmentlookup.component.js
+++ frontend/src/components/private/admin/appointmentlookup.component.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+
+import { getAppointmentsByName } from '../../../actions/adminActions';
 
 export default class AppointmentLookup extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: {},
+            results: [],
+            success: false,
             sortBy: 'Appointment Date',
             search: ''
         };
@@ -13,9 +17,21 @@ export default class AppointmentLookup extends Component {
         this.setState({search: v});
     }
     componentDidMount() {
-
+        getAppointmentsByName('').then(res => {
+            this.setState({...res});
+        });
     }
     render() {
+        const resultRows = [];
+        this.state.results.forEach((v, i) => {
+            resultRows.push(<tr key={i}>
+                <td>{i}</td>
+                <td>{v.calnetid}</td>
+                <td>{`${v.firstname} ${v.lastname}`}</td>
+                <td>{moment(v.slot).format('dddd, MMMM D h:mm A')}</td>
+                <td>{v.location}</td>
+            </tr>);
+        });
         return (
             <div className='container mt-3'>
                 <form className='mb-3 form-inline'>
@@ -37,13 +53,7 @@ export default class AppointmentLookup extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>andycate</td>
-                            <td>Andrew Doudna Cate</td>
-                            <td>Wednesday, June 10 at 10:00 AM</td>
-                            <td>Stanley Hall</td>
-                        </tr>
+                        {resultRows}
                     </tbody>
                 </table>
             </div>
