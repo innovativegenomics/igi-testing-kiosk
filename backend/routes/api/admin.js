2c0723e4d4f611673e4172d8210e07f6a6b607d7
Andy Cate <and164v@gmail.com>
2020-06-16 22:39:45 -0700

    Add schedule search

--- backend/routes/api/admin.js
+++ backend/routes/api/admin.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const pino = require('pino')({ level: process.env.LOG_LEVEL || 'info' });
-const { sequelize, Admin } = require('../../models');
+const { Sequelize, sequelize, Admin, Slot, User } = require('../../models');
+const Op = Sequelize.Op;
 
 const cas = require('../../cas');
 
@@ -21,8 +22,8 @@ router.get('/login', cas.bounce, async (request, response) => {
     const user = await Admin.findOne({where: {calnetid: calnetid}, transaction: t});
     if(user) {
       await t.commit();
-      request.session.usertype='admin';
-      request.session.adminlevel=newuser.level;
+      request.session.usertype = 'admin';
+      request.session.adminlevel = user.level;
       response.redirect('/admin/dashboard');
     } else {
       if(!uid) {
@@ -55,10 +56,80 @@ router.get('/login', cas.bounce, async (request, response) => {
     pino.error(`Could not login admin user ${calnetid} ${uid}`);
     pino.error(err);
     await t.rollback();
-    response.status(500);
+    response.status(500).send();
   }
 });
 
+router.get('/slot', cas.block, async (request, response) => {
+  const calnetid = request.session.cas_user;
+  pino.info(calnetid);
+  const level = request.session.adminlevel;
+  if(!!level && level >= 0) {
+    try {
+      const slot = await Slot.findOne({
+        where: {
+          uid: request.body.uid || ''
+        },
+        include: User
+      });
+      if(!slot) {
+        response.send({success: false});
+      } else {
+        response.send({
+          success: true,
+          slot: {
+            time: slot.time,
+            location: slot.location,
+            uid: slot.uid,
+            name: `${slot.User.firstname} ${slot.User.lastname}`,
+          },
+        });
+      }
+    } catch(err) {
+      response.status(500).send('Internal server error');
+    }
+  } else {
+    pino.info('unauthed');
+    response.status(401).send('Unauthorized');
+  }
+});
 
+router.get('/search', cas.block, async (request, response) => {
+  const calnetid = request.session.cas_user;
+  const level = request.session.adminlevel;
+  if(!!level && level >= 0) {
+    const term = request.body.term || '';
+    const users = await User.findAll({
+      where: {
+        [Op.or]: {
+          firstname: {
+            [Op.iLike]: `${term}%`,
+          },
+          lastname: {
+            [Op.iLike]: `${term}%`,
+          },
+        },
+      },
+      include: [{
+        model: Slot,
+        order: [['time', 'desc']],
+        limit: 1,
+      }]
+    });
+    const res = [];
+    users.forEach(v => {
+      res.push({
+        time: v.Slots[0].time,
+        location: v.Slots[0].location,
+        uid: v.Slots[0].uid,
+        name: `${v.firstname} ${v.lastname}`,
+      });
+    });
+    response.send({success: true, results: res});
+  } else {
+    pino.info('unauthed');
+    response.status(401).send('Unauthorized');
+  }
+});
 
 module.exports = router;
