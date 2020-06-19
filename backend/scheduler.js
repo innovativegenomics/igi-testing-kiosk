0cb4eefbc2139aa8a8609ca96866d67a8f24a7e9
Andy Cate <and164v@gmail.com>
2020-06-19 09:06:27 -0700

    Update messages

--- backend/scheduler.js
+++ backend/scheduler.js
@@ -62,7 +62,7 @@ module.exports.scheduleNewAdminEmail = async (email, uid) => {
  * @param {moment.Moment} slot - the time slot they signed up for
  */
 module.exports.scheduleAppointmentReminderEmail = async (email, slot) => {
-  await workerUtils.addJob('appointmentReminderEmail', { email: email }, {
+  await workerUtils.addJob('appointmentReminderEmail', { email: email, time: slot.toDate() }, {
     runAt: slot.clone().subtract(30, 'minute'),
     jobKey: `${email}`,
     queueName: '30minutes'
@@ -70,7 +70,7 @@ module.exports.scheduleAppointmentReminderEmail = async (email, slot) => {
 }
 
 module.exports.scheduleAppointmentReminderText = async (number, slot) => {
-  await workerUtils.addJob('appointmentReminderText', { number: number }, {
+  await workerUtils.addJob('appointmentReminderText', { number: number, time: slot.toDate() }, {
     runAt: slot.clone().subtract(30, 'minute'),
     jobKey: `${number}`,
     queueName: '30minutes'
