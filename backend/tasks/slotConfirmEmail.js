c7c6ffc1bed703b6d9a2eaecac07accc2d47142b
Andy Cate <and164v@gmail.com>
2020-06-22 12:51:03 -0700

    Update confirm email to suggest printing QR code

--- backend/tasks/slotConfirmEmail.js
+++ backend/tasks/slotConfirmEmail.js
@@ -13,7 +13,7 @@ module.exports = async (payload, helpers) => {
                   `IGI FAST Appointment ${day}`,
                   `<h1>Appointment Confirmation for ${day}</h1>
                    <h2>Please arrive at <a href='${locationLink}'>${location}</a> between ${timeStart} and ${timeEnd}</h2>
-                   <h2>Bring your phone or another device to display the QR Code below</h2>
+                   <h2>Bring your phone or another device to display the QR Code below. You can also print it out.</h2>
                    <h2>Make sure to complete the <a href='https://calberkeley.ca1.qualtrics.com/jfe/form/SV_3xTgcs162K19qRv'>campus symptom screener</a> before coming to campus and arrive wearing a mask.</h2>
                    <h2>Do not eat, drink, chew gum, or smoke for 30 minutes prior to your appointment.</h2>
                    <b><img src="cid:qrcode" style='width: 100%; max-width: 450px;'/></b>
